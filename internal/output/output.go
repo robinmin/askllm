dab1444a69ddd3f9c8b7610981aa08e37945b4f9
robinmin <luonbin@hotmail.com>
2024-07-25 15:16:30 -0700

    add new subcommand models to list all supported models

--- internal/output/output.go
+++ internal/output/output.go
@@ -28,3 +28,20 @@ func HandleOutput(outputFile, content string) error {
 	}
 	return os.WriteFile(outputFile, []byte(content), 0644)
 }
+
+func OutputMarkdown(content string) error {
+	// show markdown in console
+	r, _ := glamour.NewTermRenderer(
+		// detect background color and pick either the default dark or light theme
+		glamour.WithAutoStyle(),
+		// wrap output at specific width (default is 80)
+		glamour.WithWordWrap(120),
+	)
+	out, err := r.Render(content)
+	if err != nil {
+		log.Error(err.Error())
+		return err
+	}
+	fmt.Println(out)
+	return nil
+}
