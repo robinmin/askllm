0635d580f2116ac51a284968e5472eac6c44b672
robinmin <luonbin@hotmail.com>
2024-07-25 15:12:27 -0700

    add new subcommand models to list all supported models

--- cmd/askllm/main.go
+++ cmd/askllm/main.go
@@ -26,8 +26,8 @@ var (
 )
 
 func init() {
-	action = flag.String("a", "client", "subcommand, so far support 'client', 'server'")
-	engine = flag.String("e", "", "LLM engine (chatgpt, gemini, ollama)")
+	action = flag.String("a", "client", "subcommand, so far support 'client', 'server', 'models'")
+	engine = flag.String("e", "", "LLM engine (chatgpt, gemini, ollama, claude, groq)")
 	model = flag.String("m", "", "Model for the LLM engine")
 	configFile = flag.String("c", "~/.askllm/config.yaml", "Locatuon of configuration file")
 	promptFile = flag.String("p", "", "Prompt file")
@@ -71,48 +71,88 @@ func main() {
 
 	switch strings.ToLower(*action) {
 	case "client":
-		runClientAction(*promptFile, payload, *engine, *model, cfg)
+		err = runClientAction(*promptFile, payload, *engine, *model, cfg)
 	case "server":
-		runServerAction(*promptFile, payload, *engine, *model, cfg)
+		err = runServerAction(*promptFile, payload, *engine, *model, cfg)
+	case "models":
+		err = runModelsAction(*promptFile, payload, *engine, *model, cfg)
 	default:
 		log.Error("Invalid action: " + *action)
+		err = fmt.Errorf("invalid action: %s", *action)
 		flag.Usage()
+	}
+	if err != nil {
+		log.Error("Error: " + err.Error())
 	}
 
 	elapsedTime := time.Since(startTime)
 	log.Info(fmt.Sprintf("============== DONE ==============(%s)", elapsedTime))
 }
 
-func runClientAction(promptFile string, payload string, engine string, model string, cfg *config.Config) {
+func runClientAction(promptFile string, payload string, engine string, model string, cfg *config.Config) error {
 	// load prompt from external file (compatible with old version)
 	pt, promptText, err := prompt.GeneratePrompt(promptFile, payload)
 	if err != nil {
 		log.Error("Error getting prompt: " + err.Error())
-		return
+		return err
 	}
 
 	// // Initialize LLM engine
-	realEngine, realModel := pt.GetParameters(engine, model)
+	realEngine, realModel := pt.GetParameters(engine, model, cfg.Sys.DefaultEngine, llm.GetDefaultModel(cfg.Sys.DefaultEngine))
 	llmEngine, err := llm.NewEngine(realEngine, realModel, cfg)
 	if err != nil {
 		log.Error("Error initializing LLM engine: " + err.Error())
-		return
+		return err
 	}
 
 	// Query LLM
 	response, err := llmEngine.Query(promptText)
 	if err != nil {
 		log.Error("Error querying LLM: " + err.Error())
-		return
+		return err
 	}
 
 	// Handle output
 	if err := output.HandleOutput(*outputFile, response); err != nil {
 		log.Error("Error handling output: " + err.Error())
-		return
+		return err
 	}
+	return nil
 }
 
-func runServerAction(promptFile string, payload string, engine string, model string, cfg *config.Config) {
+func runServerAction(promptFile string, payload string, engine string, model string, cfg *config.Config) error {
 	log.Error("Not implemented")
+	return fmt.Errorf("not implemented")
+}
+
+func runModelsAction(promptFile string, payload string, engine string, model string, cfg *config.Config) error {
+	log.Info("List models for LLM engine: " + engine)
+
+	// if engine == "" {
+	// 	engine = cfg.Sys.DefaultEngine
+	// }
+	allModels, err := llm.GetAllModels(engine, cfg)
+	if err != nil {
+		log.Error("Error listing models: " + err.Error())
+		return err
+	}
+
+	var content []string
+	for engineName, modelList := range allModels {
+		content = append(content, "#### "+engineName+":\n")
+		defaultModel := llm.GetDefaultModel(engineName)
+		for _, tmpModel := range modelList {
+			if tmpModel == defaultModel {
+				content = append(content, "- [*] "+tmpModel)
+			} else {
+				content = append(content, "- "+tmpModel)
+			}
+		}
+		content = append(content, "")
+	}
+	if err := output.OutputMarkdown(strings.Join(content, "\n")); err != nil {
+		log.Error("Error in output markdown : " + err.Error())
+		return err
+	}
+	return nil
 }
